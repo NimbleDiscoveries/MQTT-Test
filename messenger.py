8d769a3203db209fb732699efe5bd41e12e8aae2
NimbleDiscoveries <53589394+NimbleDiscoveries@users.noreply.github.com>
2022-04-07 10:32:31 -0700

    Create messenger.py

---
+++ messenger.py
@@ -0,0 +1,12 @@
+import pymongo
+class cli():
+  client = pymongo.MongoClient("mongodb+srv://esp-th:Blazer1@mqtt-test.nl8qr.mongodb.net/mqtt-test?retryWrites=true&w=majority")
+  db = client.test
+  def msg_send(self,msg,type='t'):
+    if type=='t':
+      self.db.esp.insert_one({'temp':msg})
+    elif type=='h':
+      self.db.esp.insert_one({'humid':msg})
+  def __init__(self):
+    self.client = pymongo.MongoClient("mongodb+srv://esp-th:Blazer1@mqtt-test.nl8qr.mongodb.net/mqtt-test?retryWrites=true&w=majority")
+    self.db = self.client.test
