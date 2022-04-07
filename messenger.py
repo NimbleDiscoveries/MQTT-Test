c84c8a125fef8c6057e68bacdde4d4fd5f4e59e6
NimbleDiscoveries <53589394+NimbleDiscoveries@users.noreply.github.com>
2022-04-07 13:47:50 -0700

    Update messenger.py

--- messenger.py
+++ messenger.py
@@ -1,13 +1,8 @@
 import pymongo
 from time import sleep
 class cli():
-  client = pymongo.MongoClient("mongodb+srv://esp-th:Blazer1@mqtt-test.nl8qr.mongodb.net/mqtt-test?retryWrites=true&w=majority")
-  db = client.test
-  def msg_send(self,msg,type='t'):
-    if type=='t':
-      self.db.esp.insert_one({'temp':msg})
-    elif type=='h':
-      self.db.esp.insert_one({'humid':msg})
+  #client = pymongo.MongoClient("mongodb+srv://esp-th:Blazer1@mqtt-test.nl8qr.mongodb.net/mqtt-test?retryWrites=true&w=majority")
+  #db = client.test
   def __init__(self):
     sleep(20)
     print('10 more sec')
@@ -15,3 +10,9 @@ class cli():
     self.client = pymongo.MongoClient("mongodb+srv://esp-th:Blazer1@mqtt-test.nl8qr.mongodb.net/mqtt-test?retryWrites=true&w=majority")
     self.db = self.client.test
     print("setting up pymongo...")
+  def msg_send(self,msg,type='t'):
+    if type=='t':
+      self.db.esp.insert_one({'temp':msg})
+    elif type=='h':
+      self.db.esp.insert_one({'humid':msg})
+    
