ed67d1b07c96cc476db13d658889ebc316b7bfc9
NimbleDiscoveries <53589394+NimbleDiscoveries@users.noreply.github.com>
2022-04-07 13:16:11 -0700

    Update messenger.py

--- messenger.py
+++ messenger.py
@@ -1,5 +1,5 @@
 import pymongo
-
+from time import sleep
 class cli():
   client = pymongo.MongoClient("mongodb+srv://esp-th:Blazer1@mqtt-test.nl8qr.mongodb.net/mqtt-test?retryWrites=true&w=majority")
   db = client.test
@@ -9,8 +9,9 @@ class cli():
     elif type=='h':
       self.db.esp.insert_one({'humid':msg})
   def __init__(self):
-    print('hit enter')
-    input()
+    sleep(20)
+    print('10 more sec')
+    sleep(10)
     self.client = pymongo.MongoClient("mongodb+srv://esp-th:Blazer1@mqtt-test.nl8qr.mongodb.net/mqtt-test?retryWrites=true&w=majority")
     self.db = self.client.test
     print("setting up pymongo...")
