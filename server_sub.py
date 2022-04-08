f4689a8366df86c2899bc303a3038b80e7f480d9
NimbleDiscoveries <53589394+NimbleDiscoveries@users.noreply.github.com>
2022-04-08 16:59:03 -0700

    Update server_sub.py

--- server_sub.py
+++ server_sub.py
@@ -18,7 +18,7 @@ def on_subscribe(client, userdata, mid, granted_qos, properties=None):
 # print message, useful for checking if it was successful
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload)+"user data: "+str(userdata))
-    clie.msg_send(msg.payload)
+    clie.msg_send(str(msg.payload))
 
 # using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
 # userdata is user defined data of any type, updated by user_data_set()
